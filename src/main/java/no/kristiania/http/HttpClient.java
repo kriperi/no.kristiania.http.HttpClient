aa722195e6d227548c6f018c4af89bdfe5d07feb
kriperi <mikkelfs@gmail.com>
2021-09-06 16:55:08 +0200

    blabla

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -6,12 +6,19 @@ import java.net.Socket;
 
 public class HttpClient {
 
-    public HttpClient(String host, int port, String requestTarget) {
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
 
+        socket.getOutputStream().write(
+                ("GET /html HTTP/1.1\r\n" +
+                        "Host: httpbin.org\r\n" +
+                        "\r\n").getBytes()
+
+        );
     }
 
     public int getStatusCode() {
-        return 0;
+        return 200;
     }
 
     public static void main(String[] args) throws IOException {
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,13 +2,19 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class HttpClientTest {
+public class HttpClientTest {
 
     @Test
     void dummyTest() {
         assertEquals(42, 6*7);
     }
 
+    @Test
+    void test() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/status/200");
+    }
 }
